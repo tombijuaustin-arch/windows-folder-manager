0f4b70284497469c994d88b03e4aead5bbe6551f
austintombiju <tombijuaustin@gmail.com>
2026-09-19 12:56:02 +0530

    Add scripts/install.ps1

---
+++ scripts/install.ps1
@@ -0,0 +1,7 @@
+Write-Host "Installing Windows Folder Manager dependencies..."
+node --version
+npm --version
+npm install
+Write-Host ""
+Write-Host "Install complete."
+Write-Host "Start with: .\scripts\start.ps1"
